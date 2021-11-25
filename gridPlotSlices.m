b652c4a9317eae4f2131754f18d1d477c1ca33f4
Plswearpants <94640411+Plswearpants@users.noreply.github.com>
2021-11-24 16:03:51 -0800

    Update gridPlotSlices.m
    We are partway through communal editing. Note that we changed the output plot from 1 figure with multiple subplots to multiple figures.

--- gridPlotSlices.m
+++ gridPlotSlices.m
@@ -1,62 +1,46 @@
+%% Description
+% This function makes dIdV slice at a given bias. 
+%% Parameters
+% IVdata: 3D arrary(x*y*energy), Raw IV data
+% V: Vector, the meaured biases of the IVdata e.g: Vred output of "GridCorrNorm.m"
+% Biases: Vector(a scalar if it is 1D), the biases that you want to use e.g: [0.1], [0.1, 0.2]
+% Plotname: Vector of strings(same dimension as Biases), the plot names of the output plots
+
 function [] = gridPlotSlices(IVdata,V,Biases,plotname)
 
 % load colourmap
-m = 1000; % 1000 evenly spaced colour points
-cm_viridis = viridis(m); % Default matlibplot
-cm_inferno = inferno(m);
-cm_magma = magma(m);
-cm_plasma = plasma(m);
+color_scale_resolution = 1000; % 1000 evenly spaced colour points
+cm_viridis = viridis(color_scale_resolution); % Default matplotlib(for LAIR)
+cm_inferno = inferno(color_scale_resolution);
+cm_magma = magma(color_scale_resolution);
+cm_plasma = plasma(color_scale_resolution);
 
+% Homework
+% IVdata is to process the data, will be different for Createc
 IVdata = flip(permute(IVdata,[1 3 2]),2);
 
-% sxy = 0;
-% sv = 0;
-% 
-% if sxy
-%     flt = fspecial('gaussian',ceil(sxy*5),sxy);
-%     for k = 1:size(IVdata,1)
-%         IVdata(k,:,:) = imfilter(squeeze(IVdata(k,:,:)),flt,'replicate');
-%     end
-% end
-% 
-% if sv
-%     for k = 1:size(IVdata,2)
-%         for l = 1:size(IVdata,3)
-%             IVdata(:,k,l) = Gauss1d(IVdata(:,k,l),sv);
-%         end
-%     end
-% end
+%Read how many biases you want to plot 
+%N = length(Biases);
+%cols = ceil(sqrt(N))+1;
+%rows = ceil(N/cols);
 
-N = length(Biases);
-cols = ceil(sqrt(N))+1;
-rows = ceil(N/cols);
-
+% NegRamp is to determine if V is inverted, True if inverted.
 NegRamp = V(length(V))-V(1) < 0;
 
-%mi = 0;ma = max(max(max(IVdata(find(V < Biases(1),1):find(V < Biases(length(Biases)),1),:,:))));
-
-figure('Name', plotname)
-for k = 1:N
-    subplot(rows,cols,k)
-    if NegRamp
-        temp_ind = find(V < Biases(k),1);
-    else
-        temp_ind = find(V > Biases(k),1);
-    end
+% Homework: To add position to plot to manually adjust the size.
+% Plot the biases selected 
+for i = 1:length(plotname):
+    figure('Name', plotname(i))
+        if NegRamp
+             temp_ind = find(V < Biases(k),1);
+        else
+             temp_ind = find(V > Biases(k),1);
+        end
     clims = [0,3E-9];   
-    %clims = [0,5];    %for 2V pheny state on coordinated molecules 
-    %clims = [0,2.0];
-    %55:85,70:95
     imagesc(squeeze(IVdata(temp_ind,:,:)),clims)
     colorbar
     colormap(cm_magma)
-        %image(64/(ma-mi)*(squeeze(IVdata(temp_ind,:,:))-mi))
         axis image
         title([num2str(V(temp_ind)),' V'])
-%         ma = 2*mean(mean(sqrt(squeeze(IVdata(k,:,:)).^2)));mi = -ma;
-%         image(64*(squeeze(IVdata(temp_ind,:,:))-mi)/(ma-mi))
-%         axis image
-%         title([num2str(V(temp_ind)),' V'])
-%         colormap(diffcolour);
 
 end
