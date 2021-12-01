aca4f0f5fedbced8e34588ddd1d38cbe7ebf822a
Vanessa King <vanessaking0808@gmail.com>
2021-11-30 22:00:17 -0800

    current Omicron version
    Only notable difference is changing R=1 to R=2

--- gridClickForSpectrum.m
+++ gridClickForSpectrum.m
@@ -19,7 +19,7 @@ if vsmooth
     didv = smoothdata(didv, 1, 'gaussian', vsmooth);
 end
 
-% rearrange data 
+% rearrange data
 didv_flip = flip(permute(didv,[1 3 2]),2); % flip didv so that image (x,y) = (0,0) at the bottom left
 
 % use cases for different function arguments
@@ -34,14 +34,14 @@ switch nargin
         [~,imN] = min(abs(Vred-imV));
         fig_plot = squeeze(didv_flip(imN,:,:));
 end
-        
+       
 
 img = figure('Name', fig_name); imagesc(fig_plot); colormap('gray'); hold on
-axis image 
-spec = figure('Name', 'dI/dV at different points'); hold on; 
+axis image
+spec = figure('Name', 'dI/dV at different points'); hold on;
 xlabel('Bias [V]'); ylabel('dI/dV a.u.')
 
-R = 1; %defining circles that will be drawn in image
+R = 2; %defining circles that will be drawn in image
 xx = -R:.01:R;
 yy = sqrt(R^2-xx.^2);
 
@@ -51,7 +51,7 @@ for k = 1:n
     pos = round(ginput(1)); %click where you want the spectrum
     plot(pos(1)+xx,pos(2)+yy,colours(mod(k-1,7)+1))
     plot(pos(1)+xx,pos(2)-yy,colours(mod(k-1,7)+1))
-    
+   
     figure(spec)
     plot(Vred,squeeze(didv_flip(:,pos(2),pos(1)))+(k-1)*offset,colours(mod(k-1,7)+1))
 end
