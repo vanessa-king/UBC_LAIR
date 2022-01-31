3a26406631f051b51ba975144a55d509c96c579d
Plswearpants <94640411+Plswearpants@users.noreply.github.com>
2022-01-31 15:59:11 -0800

    Update topoPlaneSub.m

--- topoPlaneSub.m
+++ topoPlaneSub.m
@@ -1,11 +1,15 @@
+% Description :  
+% what does the function do : Subtracts the plane in topography images, local flat/global flat
+% Parameters
+%%  Input parameters: 
+    % image(structure): the structure file generated from topoLoadData.m 
+    % n(integer): number of points to sample, the default number is 200 
+    % plot(boolean): choose to plot the process of plane sub, the is default is F 
+%%  Output parameters: 
+    % topo: Outputs a single z matrix with the plane subtracted.
+
+
 function topo = topoPlaneSub(image,n,plot)
-%TOPOPLANESUB Subtracts the plane in topography images from Omicron STM.--- Q
-%   Takes an image, and randomly sample n points. A plane is fitted to these points, and subtracted from the image. 
-%   image - the structure file generated from topoLoadData.m (structure)
-%   n - number of points to sample, the default number is 200 (integer)
-%   plot - choose to plot the process of plane sub, the is default is F (boolean)----Q
-%
-%   Outputs a single z matrix with the plane subtracted.
 
 % select mode of operation
 if nargin == 1
@@ -14,14 +18,13 @@ elseif nargin == 2
     plot = 0;
 end
     
-% auto plane subtract
 % subtract a plane
-xsamp = round(rand(n,1)*(numel(image.x_img)-1))+1;
-ysamp = round(rand(n,1)*(numel(image.y_img)-1))+1;
-zsamp = diag(image.z_img(xsamp, ysamp));
+xsample = round(rand(n,1)*(numel(image.x_img)-1))+1;
+ysample = round(rand(n,1)*(numel(image.y_img)-1))+1;
+zsample = diag(image.z_img(xsample, ysample));
 
 %start the fitting
-f = fit([xsamp, ysamp], zsamp, 'poly11'); % do a fit for a plane
+f = fit([xsample, ysample], zsample, 'poly11'); % do a fit for a plane, poly11 is for 2D 1st order polynomial 
 [Y,X] = meshgrid(1:numel(image.y_img), 1:numel(image.x_img));
 plane = f.p00 + f.p10*X + f.p01*Y;
 topo = imsubtract(image.z_img,plane); % new topography image
