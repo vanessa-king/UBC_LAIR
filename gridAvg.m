ab44a09f28010806e3f79c81eb5df3e29e43bea2
jamesday76 <94569462+jamesday76@users.noreply.github.com>
2022-01-31 13:35:30 -0800

    Update gridAvg.m

--- gridAvg.m
+++ gridAvg.m
@@ -1,22 +1,21 @@
 %
 % DESCRIPTION
-% This function takes the average of the iv_data and plots it. This is called in the Plot 1D Density section.
+% This function takes the average of the iv_data and plots it. (For Tesla, this is called in the Plot 1D Density section.)
 %
 % PARAMETERS
-%   num_layer: comes from SOMEWHERE
 %
-%   iv_data size: num_layer x grid size x grid size
-%   V size: num_layer x 1
+%   iv_data size: number_bias_layer x grid size x grid size
+%   V size: number_bias_layer x 1
 %
 
 function [avg_iv_data] = gridAvg(iv_data, V)
 
 %
 % size(V) returns a row vector whose elements are the lengths of the corresponding dimensions of V
-% avg_iv_data is the mean of iv_data based on the dimensions specified in the vector [a b]
+% avg_iv_data is the mean of iv_data based on the dimensions specified in the vector [a b] - here, [3 2] is the total grid size and the order of those numbers probably does not matter.
 %
 
-[elayer, ~] = size(V);
+[number_bias_layer, ~] = size(V);
 avg_iv_data = mean(iv_data, [3 2]);
 
 %
@@ -24,7 +23,7 @@ avg_iv_data = mean(iv_data, [3 2]);
 %
 
 figure();
-plot(V,reshape(avg_iv_data(:),elayer,1))
+plot(V,reshape(avg_iv_data(:),number_bias_layer,1))
 xlabel("V")
 ylabel("average iv data")
 end
