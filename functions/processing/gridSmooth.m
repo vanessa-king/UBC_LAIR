0b2d3279222b1a03ad664bbd196985d98930be61
jamesday76 <94569462+jamesday76@users.noreply.github.com>
2023-04-27 16:48:51 -0700

    Update gridSmooth.m

--- functions/processing/gridSmooth.m
+++ functions/processing/gridSmooth.m
@@ -1,10 +1,8 @@
-function [smooth_iv,comment] = gridSmooth(iv,time)
+function [smooth_iv] = gridSmooth(iv,time)
 %GRIDSMOOTH Applies moving-average smoothin on didv data vs. time.
 %   iv = grid of iv data (3d matrix). Size n x length(time) x
 %   length(time)
 %   time = independent variable axis to plot didv against
-
-comment = sprintf("gridSmooth(iv=%s, time=%s)|", iv, time);
 
 smooth_iv = zeros(size(iv));
 [~, second_dim, third_dim] = size(iv);
