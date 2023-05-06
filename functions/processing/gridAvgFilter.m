b758ffc18a9197f176f6bc82cbbc4bacfbb393c2
m-altthaler <108487905+m-altthaler@users.noreply.github.com>
2023-05-05 17:12:10 -0700

    MA-2023-05-05_V3
    functions\processing\gridAvgFilter.m updated
    internal variable explanations missing!

--- functions/processing/gridAvgFilter.m
+++ functions/processing/gridAvgFilter.m
@@ -1,6 +1,17 @@
-function [avg_iv] = gridAvgFilter(iv_data, V, filter_indices)
+function [avg_iv, comment] = gridAvgFilter(iv_data, V, filter_indices)
 %GRIDAVGFILTER Finds average didv with index filter applied
 %   filter_indices = 1D array of indices in iv grid to average over
+
+% missing explanation of variables! 
+
+%output format for comment: "<function>(<VAR1>=<VAR1_value>,<VAR2>=<VAR2_value>,<VAR3>,...,)|"  
+%Never plot data (e.g. the whole gird) in the comment, only plot the values
+%('=<VARn_value>') of variables that decide/affect how the function
+%processes data (e.g. order of fit, ...) 
+%Note convert all <VARn_value> to strings; 
+comment = sprintf("gridAvgFilter(iv_data, V, filter_indices)|");
+
+
 [elayer, ~] = size(V);
 
 iv_data_flip = flip(permute(iv_data,[1 3 2]),2); % flip didv so that image (x,y) = (0,0) at the bottom left
