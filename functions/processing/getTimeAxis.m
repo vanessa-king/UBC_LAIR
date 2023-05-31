82f6f209100116c6da2f0b5d774894644bedbcfb
jamesday76 <94569462+jamesday76@users.noreply.github.com>
2023-05-31 09:07:52 -0700

    comment added for logging
    Comment generated within the function and then added to the logging file.

--- functions/processing/getTimeAxis.m
+++ functions/processing/getTimeAxis.m
@@ -1,7 +1,9 @@
-function [time] = getTimeAxis(pointsPerSweep, Traster)
+function [time,comment] = getTimeAxis(pointsPerSweep, Traster)
 %GETTIMEAXIS Creates a time axis to plot against didv or i(v)
 %   pointsPerSweep = number of points per sweep
 %   Traster = seconds per sample
+
+comment = sprintf("getTimeAxis(pointsPerSweep=%s, Traster=%s)|", pointsPerSweep, Traster);
 
 time = (0:pointsPerSweep-1)*Traster;
 
--- functions/processing/gridSmooth.m
+++ functions/processing/gridSmooth.m
@@ -1,8 +1,10 @@
-function [smooth_iv] = gridSmooth(iv,time)
+function [smooth_iv,comment] = gridSmooth(iv,time)
 %GRIDSMOOTH Applies moving-average smoothin on didv data vs. time.
 %   iv = grid of iv data (3d matrix). Size n x length(time) x
 %   length(time)
 %   time = independent variable axis to plot didv against
+
+comment = sprintf("smooth_iv(iv, time=%s)|", time);
 
 smooth_iv = zeros(size(iv));
 [~, second_dim, third_dim] = size(iv);
