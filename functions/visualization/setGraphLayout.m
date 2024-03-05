6769e22ca8872348a47faa59d577b82f86a8f663
jsk2355 <94018834+jsk2355@users.noreply.github.com>
2024-03-04 18:34:12 -0800

    minor tweaks and more commenting

--- functions/visualization/setGraphLayout.m
+++ functions/visualization/setGraphLayout.m
@@ -20,8 +20,6 @@ switch LayoutCase
         ax.XLabel.String = 'bias(V)';
         ax.YLabel.String = 'dI/dV[a.u.]';
         ax.XTick = [-0.04 -0.02 0 0.02 0.04];        
-        ax.XLim = [-0.04 0.04];
-        ax.YLim = [0 3e-9];
     case "IV_fwdbwd"
         ax.FontSize = 20;
         ax.XLabel.String = 'bias(V)';
