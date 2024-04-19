78ec878cd1c50ca550f5e8c32cb32a4304ff1dbb
m-altthaler <108487905+m-altthaler@users.noreply.github.com>
2024-04-19 09:29:10 -0700

    MA-setLogFile comment fix

--- functions/basic/setLogFile.m
+++ functions/basic/setLogFile.m
@@ -1,8 +1,9 @@
 function [LOGpath,LOGfile] = setLogFile(fileBool)
 %UI to select file / directory + set name for LOGpath and LOGfile 
-%   User input via UI to select a folder (file). If no file is picked the
-%   user is prompted to set a file name which is set to be unique via
-%   appending a running number. The optional variable 
+%   User input via UI to select a file (folder). If no file is picked the
+%   user is prompted to set a file name, which is set to be unique via
+%   appending a running number. The optional variable fileBool defines if a
+%   file is chosen or a folder. It defaults to chosing a folder.
 arguments
     fileBool logical =0
 end
