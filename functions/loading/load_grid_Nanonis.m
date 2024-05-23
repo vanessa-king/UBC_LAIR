5945aa134491a1334601fbbf1140c36db171e40e
vanessa <vanessaking0808@gmail.com>
2024-05-23 16:40:13 -0700

    Formatting 1d variables and x,y values

--- functions/loading/load_grid_Nanonis.m
+++ functions/loading/load_grid_Nanonis.m
@@ -29,21 +29,37 @@ comment = sprintf("load_grid_Nanonis(folder=%s, gridFileName=%s)|", folder, grid
 %Return the entire header, in case we need it
 grid.header = header;
 
-
+%Calculate the x and y values from the header
 %Get x_position and y_position (in m) off of parameters in the header:
 grid.x_position = header.grid_settings(1); 
 grid.y_position = header.grid_settings(2); 
+x_range = header.grid_settings(3);%in m
+y_range = header.grid_settings(4);%in m
+x_range = x_range * 1e9; %convert m to nm
+y_range = y_range * 1e9; %convert m to nm
+number_x_points = header.grid_dim(1);
+number_y_points = header.grid_dim(2);
+
+%Get x out of the experimental parameters:
+grid.x = transpose(linspace((-x_range/2.0), (x_range/2.0), number_x_points));
+
+%Get y out of the experimental parameters:
+if par{1,1}(4) < grid.y_position %y direction is up
+    y_all = transpose(linspace((-y_range/2.0), (y_range/2.0), number_y_points));
+elseif par{1,1}(4) > grid.y_position %y direction is down
+    y_all = transpose(linspace((y_range/2.0), (-y_range/2.0), number_y_points));
+else
+    fprintf('Invalid scan direction in header.\n');
+    return
+end
 
 %Calculate V based off of parameters in the header:
 bias_start = par{1,1}(1);
 bias_end = par{1,1}(2);
 number_bias_points = header.points;
-V = linspace(bias_start,bias_end,number_bias_points);
-grid.V = transpose(V);%(1, number_bias_points) -> (number_bias_points, 1)
+grid.V = transpose(linspace(bias_start,bias_end,number_bias_points));
 
 %Get the channels from the data: 
-number_x_points = header.grid_dim(1);
-number_y_points = header.grid_dim(2);
 I_all = zeros(number_x_points, number_y_points, number_bias_points);
 lock_in_all = zeros(number_x_points, number_y_points, number_bias_points);
 I_backward_all = zeros(number_x_points, number_y_points, number_bias_points);
@@ -75,28 +91,6 @@ end
 z_all = z_all(:,:,1);
 z_backward_all = z_backward_all(:,:,1);
 
-%Get x out of the experimental parameters:
-x = linspace(bias_start,bias_end,number_x_points);
-grid.x = transpose(x);%(1, number_x_points) -> (number_x_points, 1)
-%Convert from m to nm
-grid.x = grid.x .* 1e9; 
-
-%Calculate the x and y values from the header
-x_range = header.scan_range(1); %in m
-y_range = header.scan_range(2); %in m
-x_range = x_range * 1e9; %convert m to nm
-y_range = y_range * 1e9; %convert m to nm
-x_resolution = header.scan_pixels(1);
-y_resolution = header.scan_pixels(2);
-topo.x = linspace((-x_range/2.0), (x_range/2.0), x_resolution);
-if topo.header.scan_dir == "up" %y direction is positive
-    y_all = linspace((-y_range/2.0), (y_range/2.0), y_resolution);
-elseif topo.header.scan_dir == "down" %y direction is negative
-    y_all = linspace((y_range/2.0), (-y_range/2.0), y_resolution);
-else
-    fprintf('Invalid scan direction in header.\n');
-    return
-end
 
 % This section is to determine if we have a partial image and remove 0 values if so. 
 % Note this wasn't necessary for x or V since they're always full
@@ -125,13 +119,8 @@ if all(I_all,'all') %grid is finished
         grid.other_channel = other_channel_all;
     end
     
-    %Get y out of the experimental parameters:
-    grid.y = zeros([number_y_points,1]);
-    for j = 1:number_y_points
-        grid.y(j) = par{1,j}(4);
-    end
-    %Convert from m to nm
-    grid.y = grid.y .* 1e9;
+    %Assign y:
+    grid.y = y_all;
 
 else  %grid is not finished, take off the any unfinished fast scan line
     grid.I_all = I_all;
@@ -168,13 +157,9 @@ else  %grid is not finished, take off the any unfinished fast scan line
         grid.other_channel = grid.other_channel_all(:, 1:max(y_coordinates)-1, :);
     end
 
-    %Get y out of the experimental parameters:
-    grid.y = zeros([max(y_coordinates)-1, 1]);
-    for j = 1:max(y_coordinates)-1
-        grid.y(j) = par{1,j}(4);
-    end
-    %Convert from m to nm
-    grid.y = grid.y .* 1e9;
+    %Assign y:
+    grid.y_all = y_all;
+    grid.y = grid.y_all(1:max(y_coordinates)-1);
 
 end
 
