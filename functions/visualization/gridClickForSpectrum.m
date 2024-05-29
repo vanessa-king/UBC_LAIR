16fb6b228fec3fc102383cccb2019ed0660b6a9c
m-altthaler <108487905+m-altthaler@users.noreply.github.com>
2024-05-28 17:12:40 -0700

    --patch_02
    updated comment
    added option to parse point list to reproduce the functions behavior

--- functions/visualization/gridClickForSpectrum.m
+++ functions/visualization/gridClickForSpectrum.m
@@ -1,19 +1,30 @@
-function [comment] = gridClickForSpectrum(didv, V_reduced, imageV, n, offset)
+function [comment] = gridClickForSpectrum(didv, V_reduced, imageV, offset, n, pointsList)
 %creates a GUI window where you can select a point(s), then it plots the spectra from that point(s). 
-%   
+%   Plots a 2D slice of the didv data at the closest value of V_reduced to 
+%   the set value imageV. From this figure up to n points can be selected 
+%   by clicking on the 2D image. For the selected points the spectra didv(V) 
+%   are plotted. 
+%
+% Edited: M. Altthaler 05/2024; Vanessa 2023
+%
+%   OPT TBD:    make fucntion accept a list of point coordinates to be
+%               plotted. This feature is untested! (05/2024)
+%
 %Arguments:
 %   didv:       3D Matrix with dI/dV data in the format: (x,y,V) 
 %   V_reduced:  reduced vector with bias voltages (see gridCorrNorm for definition of V_reduced)
 %   imageV:     float, Voltage at which to display image
-%   n:          integer, Number of point spectra to plot
 %   offset:     Vertical offset for each point spectra 
+%   n:          integer, Number of point spectra to plot (optional, default: 2)
+%   pointsList: list of (x,y) coordinates in nx2 format (optional, default: [])
 
 arguments
     didv        {mustBeNumeric}
     V_reduced   {mustBeVector}
     imageV      {mustBeNumeric}
-    n           {mustBeNumericOrLogical}
     offset      {mustBeNumeric}
+    n           {mustBeNumericOrLogical}=2
+    pointsList  {mustBeNumeric}=[]
 end
 
 %logComment
@@ -45,14 +56,43 @@ radius = 2;
 xx = -radius:.01:radius;
 yy = sqrt(radius^2-xx.^2);
 
-colours = 'rgbcmyk'; % A list of the colours to be used to represent the different points (up to 7)
+% A list of the colours to be used to represent the different points (up to 7)
 %for loop for every point clicked.
-for k = 1:n
-    figure(img)
-    position = round(ginputAllPlatform(1)); %click where you want the spectrum
-    plot(position(1)+xx,position(2)+yy,colours(mod(k-1,7)+1))
-    plot(position(1)+xx,position(2)-yy,colours(mod(k-1,7)+1))
-   
-    figure(spec)
-    plot(V_reduced,squeeze(didv(position(1),position(2),:))+(k-1)*offset,colours(mod(k-1,7)+1))
+colours = 'rgbcmyk'; 
+
+%init append (x,y) coordinates to the comment
+commentAppend = 'selceted points (x,y): ';
+comment = strcat(comment,commentAppend);
+
+
+if isempty(pointsList)
+    %click for points
+    for k = 1:n
+        figure(img)
+        position = round(ginputAllPlatform(1)); %click where you want the spectrum
+        plot(position(1)+xx,position(2)+yy,colours(mod(k-1,7)+1))
+        plot(position(1)+xx,position(2)-yy,colours(mod(k-1,7)+1))
+        
+        %appends clicked position to the log comment
+        commentAppend = sprintf('(%d,%d), ',position(1),position(2));
+        comment = strcat(comment,commentAppend);
+       
+        figure(spec)
+        plot(V_reduced,squeeze(didv(position(1),position(2),:))+(k-1)*offset,colours(mod(k-1,7)+1))
+    end
+else
+    %plot pointsList
+    for k = 1:lenght(pointsList)
+        figure(img)
+        position = pointsList(k,:); %assing k-th point of the list to position instead of klicking
+        plot(position(1)+xx,position(2)+yy,colours(mod(k-1,7)+1))
+        plot(position(1)+xx,position(2)-yy,colours(mod(k-1,7)+1))
+        
+        %appends clicked position to the log comment
+        commentAppend = sprintf('(%d,%d), ',position(1),position(2));
+        comment = strcat(comment,commentAppend);
+       
+        figure(spec)
+        plot(V_reduced,squeeze(didv(position(1),position(2),:))+(k-1)*offset,colours(mod(k-1,7)+1))
+    end
 end
