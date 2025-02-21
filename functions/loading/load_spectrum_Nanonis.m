1aaf5b1413105ae36f3feb69716fd36a299e2999
vanessa <vanessaking0808@gmail.com>
2025-02-20 17:09:41 -0800

    Minor edits and moving helper functions into existing files

--- functions/loading/load_spectrum_Nanonis.m
+++ functions/loading/load_spectrum_Nanonis.m
@@ -1,4 +1,4 @@
-function [spectrum,comment] = load_spectrum_Nanonis(folder,spectrumFileName)
+function spectrum = load_spectrum_Nanonis(folder,spectrumFileName)
 %Wrapper load function for point spectrum '.dat' files
 %   Uses specLoad.m function, processes the data into a structure 
 % Input: 
@@ -6,19 +6,11 @@ function [spectrum,comment] = load_spectrum_Nanonis(folder,spectrumFileName)
 %   spectrumFileName: the filename, including the extension
 % Output: 
 %   spectrum: structure containing all the point spectrum associated data
-%   comment: string containing log comment
 
 arguments
     folder              {mustBeText}
     spectrumFileName    {mustBeText}
 end
-
-%output format for comment: "<function>(<VAR1>=<VAR1_value>,<VAR2>=<VAR2_value>,<VAR3>,...,)|"  
-%Never plot data (e.g. the whole gird) in the comment, only plot the values
-%('=<VARn_value>') of variables that decide/affect how the function
-%processes data (e.g. order of fit, ...) 
-%Note convert all <VARn_value> to strings; 
-comment = sprintf("load_spectrum_Nanonis(folder=%s, spectrumFileName=%s)|", folder, spectrumFileName);
 
 %regular function processing:
 
@@ -47,3 +39,65 @@ end
 
 
 end
+
+function [header, channels, data] = specLoad(fileName)
+%Nanonis-made DAT ASCII file loader
+% Input:  
+%   fileName: string of the file name
+% Output:
+%   header: structure containing all header information from the file
+%   channels: array containing string titles of each channel
+%   data: array containing the dataset in shape [channels, energy]
+
+data=''; header=''; channels='';
+
+if exist(fileName, 'file')
+    fid = fopen(fileName, 'r', 'ieee-be');    % open with big-endian
+else
+    fprintf('File does not exist.\n');
+    return;
+end
+
+% read header data
+% The header consists of key-value pairs, separated by a tab character
+% e.g. Settling time (s)	200E-6	
+% The beginning of the data block starts with [DATA]
+while 1
+    s = strtrim(fgetl(fid));
+    if strcmp(upper(s),'[DATA]')
+        break
+    end
+    
+    %s1 = strsplit(s,char(9));  % not defined in older Matlab versions
+    s1 = strsplit_i(s,char(9));
+    s1_s = prod(size(s1));
+    if s1_s > 0 % line contains a tab
+        s_key = strrep(lower(s1{1}), ' ', '_');
+        s_val = '';
+        if s1_s > 1
+            s_val = strrep(s1{2}, '"', '');
+        end
+        
+        s_key = regexprep(lower(s_key), '[^a-z0-9_]', '_');
+        header.(s_key) = s_val;
+    end
+end
+
+% read the channel names
+s = strtrim(fgetl(fid));
+%channels = strsplit(s,'=');  % not defined in older Matlab versions
+channels = strsplit_i(s,char(9));
+
+% read the data
+data = fscanf(fid, '%f', [prod(size(channels)) Inf]);
+fclose(fid);
+end
+
+function s = strsplit_i(str, delim)
+    s = {};
+    while ~isempty(str),
+        [t,str] = strtok(str, delim);
+        s{end+1} = t;
+    end
+end
+
